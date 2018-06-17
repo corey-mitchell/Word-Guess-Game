743c60a19b445e6ceb9592d24311343c652ae520
Macaroni Biff <corey.waitforit.mitchell@gmail.com>
2018-06-17 13:26:28 -0500

    making a little progress.

---
+++ assets/Javascript/game.js
@@ -0,0 +1,89 @@
+var validKeys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var lives = 10;
+var guesses = [];
+var wordChoices = ["batman", "superman", "spiderman" ];
+var currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+var hyphens = [];
+
+for (var i = 0; i<currentWord.length; i++){
+    hyphens.push("_")
+};
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+   
+
+    if(validKeys.indexOf(userGuess) !== -1){   
+        
+        if (userGuess === currentWord.charAt([0])){
+            hyphens.splice(0, 1, userGuess)
+        }
+
+        else if (userGuess === currentWord.charAt([1])){
+            hyphens.splice(1, 1, userGuess)
+        }
+        
+        else if (userGuess === currentWord.charAt([2])){
+            hyphens.splice(2, 1, userGuess)
+        }
+
+        else if (userGuess === currentWord.charAt([3])){
+            hyphens.splice(3, 1, userGuess)
+        }
+
+        else if (userGuess === currentWord.charAt([4])){
+            hyphens.splice(4, 1, userGuess)
+        }
+
+        else if (userGuess === currentWord.charAt([5])){
+            hyphens.splice(5, 1, userGuess)
+        }
+
+        else if (userGuess === currentWord.charAt([6])){
+            hyphens.splice(6, 1, userGuess)
+        }
+
+        else if (userGuess === currentWord.charAt([7])){
+            hyphens.splice(7, 1, userGuess)
+        }
+
+        else if (guesses.indexOf(userGuess) !== -1) {
+            return;
+        }
+
+        else {
+            lives--;
+            guesses.push(userGuess);
+
+        }
+    
+        if(lives === 0) {
+            guesses.length = 0;
+            lives = 10;
+            currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+        }
+
+
+    };
+
+
+    html = 
+    "<h1>Press Any Key to Get Started!</h1>" +
+
+    "<h2>Wins: " + wins + "</h2>" +
+
+    "<h2>Current Word</h2>" +
+
+    hyphens +
+
+    "<h2>Number of Guesses Remaining: " + lives + "</h2>" +
+
+    "<h2>Letters Already Used: " + guesses + "</h2>"
+
+    document.querySelector("#text").innerHTML = html;
+   
+   
+   
+
+};
