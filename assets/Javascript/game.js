7ef689f23006e3f631b61596d65b130e8e7edd6a
Macaroni Biff <corey.waitforit.mitchell@gmail.com>
2018-06-18 18:40:03 -0500

    added comments and cleaned up the code a little.

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,3 +1,4 @@
+//setting initial variables
 var validKeys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var lives = 10;
@@ -6,99 +7,61 @@ var wordChoices = ["batman", "superman", "spiderman" ];
 var currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 var hyphens = [];
 
-
-
-
+//creating hyphens as filler for unguesses letters
 for (var i = 0; i<currentWord.length; i++){
     hyphens[i] = "_" ;
 };
     
+var answer = hyphens.join(" ")
+document.querySelector("#answer").innerHTML = answer
 
-
-
+//main function
 document.onkeyup = function(event) {
     var userGuess = event.key;
 
+    //limiting user guess to alphabetic characters
     if(validKeys.indexOf(userGuess) !== -1){ 
       
-        var letter = userGuess;
+        //if user guesses correctly, this replaces hyphen index with correct letter guess
         for (var i = 0; i < currentWord.length; i++) {
-            if (currentWord[i] === letter){
-                hyphens[i] = letter;
-            }
-        }
-        
-        // if (userGuess === currentWord.charAt([0])){
-        //     hyphens.splice(0, 1, userGuess)
-        // }
+            if (currentWord[i] === userGuess){
+                hyphens[i] = userGuess;
+            };
+        };
 
-        // else if (userGuess === currentWord.charAt([1])){
-        //     hyphens.splice(1, 1, userGuess)
-        // }
-        
-        // else if (userGuess === currentWord.charAt([2])){
-        //     hyphens.splice(2, 1, userGuess)
-        // }
-
-        // else if (userGuess === currentWord.charAt([3])){
-        //     hyphens.splice(3, 1, userGuess)
-        // }
-
-        // else if (userGuess === currentWord.charAt([4])){
-        //     hyphens.splice(4, 1, userGuess)
-        // }
-
-        // else if (userGuess === currentWord.charAt([5])){
-        //     hyphens.splice(5, 1, userGuess)
-        // }
-
-        // else if (userGuess === currentWord.charAt([6])){
-        //     hyphens.splice(6, 1, userGuess)
-        // }
-
-        // else if (userGuess === currentWord.charAt([7])){
-        //     hyphens.splice(7, 1, userGuess)
-        // }
-
-        // else if (userGuess === currentWord.charAt([8])){
-        //     hyphens.splice(8, 1, userGuess)
-        // }
-
-        // else if (guesses.indexOf(userGuess) !== -1) {
-        //     return;
-        // }
-
-        // else {
+        //if user guesses incorrectly, this counts down remainging guesses and pushes incorrect guess to the guesses array
+        // if (currentWord.indexOf(userGuess) !== -1) {
         //     lives--;
         //     guesses.push(userGuess);
+        // };
 
-        // }
+        //stops user from guessing the same thing again
+        if (guesses.indexOf(userGuess) !== -1) {
+            return;
+        };
 
+        //had to add this line of code in later so that the answer would update as the letter is selected.
+        answer = hyphens.join (" ");
 
-        var answer = hyphens.join (" ");
-
-
-        if(answer == currentWord) {
-            currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+        //sets up win scenario
+        if(currentWord === hyphens.join("")) {
+            // currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
             wins ++;
             lives = 10;
             guesses.length = 0;
-        }
+        };
     
+        //sets up loss scenario
         if(lives === 0) {
+            // currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
             guesses.length = 0;
             lives = 10;
-            currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-        }
-
-
-
-
+        };
 
     };
 
-
-    html = 
+    //rewrites html on page
+    var html = 
     "<h1>Press Any Key to Get Started!</h1>" +
 
     "<h2>Wins: " + wins + "</h2>" +
@@ -112,17 +75,9 @@ document.onkeyup = function(event) {
     "<h2>Letters Already Used: " + guesses + "</h2>"
 
     document.querySelector("#text").innerHTML = html;
-   
-   
-   
 
 };
 
-// function letter(){
-//     var letter = userGuess;
-//     for (var i = 0; i < currentWord.length; i++) {
-//         if (currentWord[i] === letter){
-//             hyphens[i] = letter;
-//         }
-//     }
-//  }
+
+//I was having a lot of issues with limiting character sets down. Which is why I created a 'validKey' array.
+//I am going to keep trying, but if you are reading this than I have failed miserably :(
